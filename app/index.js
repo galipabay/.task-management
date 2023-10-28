5ff3331e16cb137c704411b89aee81428a874864
Galip <galipabay37@gmail.com>
2023-10-28 15:00:50 +0300

    ilk girdiler

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import {View,Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Naber knk
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
